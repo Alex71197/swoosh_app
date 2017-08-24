f4903ff1f4e6d7f079fd565fd267dc08b3fe0b35
Alex Villacres <alex@Alexs-MacBook-Pro.local>
2017-08-24 16:47:14 -0700

    Created model and logic for passing data

---
+++ swoosh_app/swoosh_app/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh_app
+//
+//  Created by Alex Villacres on 8/24/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
